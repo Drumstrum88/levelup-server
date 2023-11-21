54fd5b267bcaa9d1732ba8e3e49a595b450832f7
drumstrum88 <rev.jsanderson@gmail.com>
2023-11-20 19:43:17 -0600

    adding authentication

--- levelup/urls.py
+++ levelup/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from levelupapi.views import GameTypeView, EventView, GameView
+from django.urls import path
+from levelupapi.views import register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
@@ -28,4 +30,6 @@ router.register(r'games', GameView, 'game')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('register', register_user),
+    path('checkuser', check_user),
 ]
